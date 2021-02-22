ab65d5c41a065b56b2b82522fb1518117e3e5efc
Jaqueline L. Botaro <jlemesbotaro@gmail.com>
2021-02-22 16:12:16 -0300

    Add missing file and improve gitignore

---
+++ UILabel+Ext.swift
@@ -0,0 +1,9 @@
+//
+//  UILabel+Ext.swift
+//  EasyRecipe
+//
+//  Created by Jaqueline Lemes Botaro on 09/11/20.
+//  Copyright © 2020 Jaqueline Lemes Botaro. All rights reserved.
+//
+
+import Foundation
